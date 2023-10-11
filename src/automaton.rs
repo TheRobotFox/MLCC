3408de6a5a8dcb33bf09ef0a630a263a37c0587f
Tomke <pfoch.tomke@gmail.com>
2023-10-11 20:51:14 +0200

    Trace Return

--- src/automaton.rs
+++ src/automaton.rs
@@ -60,6 +60,7 @@ macro_rules! make_automanton {
             $($name: HashMap<$f, usize>,)*
             rules: &'a Vec<parser::Rule>,
             state_map: HashMap<StateImpl, IdxState>,
+            goto_states: Vec<(StateImpl, HashMap<ReductendPosition, Positions>)>
             // optional merge by tokens ( tails )
         }
         impl Automaton {
@@ -74,6 +75,7 @@ macro_rules! make_automanton {
                     $($name: HashMap::new(),)*
                     rules: lr.rules,
                     state_map: HashMap::new(),
+                    goto_states: Vec::new()
                 };
                 builder.run(lr)
             }
@@ -138,9 +140,52 @@ impl AutomatonBuilder<'_> {
             goto: goto.into_iter().collect()
         };
 
-        self.impl_state(lr, state_impl);
+        let _start = self.impl_state(lr, state_impl, &mut Vec::new())?;
+
+
         Ok(self.automaton)
     }
+
+    fn compare_gotos(a: &BTreeMap<ReductendPosition, Positions>, b: &BTreeMap<ReductendPosition, Positions>) -> bool {
+
+        for (reductend, position) in a {
+            let got = match b.get(reductend) {
+                Some(p) =>p,
+                None => return false
+            };
+            if got != position {
+                return false
+            }
+        }
+        return true
+    }
+    fn get_compatable_goto(&self, state_impl: StateImpl) -> IdxState {
+        for (imp, idx) in &self.state_map {
+            if imp.position != state_impl.position {continue}
+            if Self::compare_gotos(&imp.goto, &state_impl.goto) { return *idx }
+        }
+        panic!("search: {:#?}\ngot: {:#?}", state_impl, self.state_map)
+    }
+
+    fn impl_goto(&mut self, lr: &LR, state_impl: StateImpl, gotos: HashMap<ReductendPosition, Positions>) -> Result<(), Error>{
+        let idx = *self.state_map.get(&state_impl).unwrap();
+        let state_ref = lr.state_map.get(&state_impl.position).unwrap();
+
+        for (reductend, return_position) in &state_ref.goto_map {
+            let return_impl = StateImpl{
+                goto: state_impl.goto.clone(),
+                position: return_position.clone()
+            };
+
+            let return_idx = self.get_compatable_goto(return_impl);
+            let reduction = self.make_reduction(reductend.clone().clone())?;
+
+            let state = self.automaton.states.get_mut(idx).unwrap();
+            state.goto.insert(reduction, return_idx);
+        }
+        Ok(())
+    }
+
     fn import_next(lr: &LR, postition: &Positions, goto: &BTreeMap<ReductendPosition, Positions>, visited: &mut HashSet<Positions>) -> Vec<Token> {
 
         visited.insert(postition.clone());
@@ -154,7 +199,7 @@ impl AutomatonBuilder<'_> {
         }
         tokens
     }
-    fn impl_state(&mut self, lr: &LR, state_impl: StateImpl) -> Result<IdxState, Error> {
+    fn impl_state(&mut self, lr: &LR, state_impl: StateImpl, parrent_goto: &mut Vec<(IdxReduction, IdxState)>) -> Result<IdxState, Error> {
         // check implemented
         let impl_idx = match self.state_map.entry(state_impl.clone()) {
             Entry::Occupied(e) => return Ok(*e.get()),
@@ -169,32 +214,12 @@ impl AutomatonBuilder<'_> {
         let mut state = State::default();
         state.position = state_impl.position.clone();
 
-        let mut return_goto = state_impl.goto.clone();
-        return_goto.remove(reduce);
-
-        for (reductend, position) in &state_ref.goto_map {
-            let return_impl = StateImpl{
-                position: position.clone(),
-                goto:
-            };
-            let reduction = self.make_reduction(reductend.clone())?;
-            state.goto.insert()
-        }
-
-        for (token, position) in &state_ref.shift_map {
-            // implement next state
-            let next_impl = StateImpl {
-                position: position.clone(),
-                goto: state_ref.goto_map.clone().into_iter().collect()
-            };
-            let next = self.impl_state(lr, next_impl)?;
-
-            let t = vecmap!(self, terminals, token.clone());
-            state.lookahead.insert(t, Action::Shift(next));
-        }
         if let Some(reduce) = &state_ref.reduce {
 
             let return_position = state_impl.goto.get(reduce).unwrap_or(&lr.x);
+
+            let mut return_goto = state_impl.goto.clone();
+            return_goto.remove(reduce);
 
             // get imports
             let tokens = Self::import_next(lr, return_position, &return_goto, &mut HashSet::new());
@@ -203,9 +228,11 @@ impl AutomatonBuilder<'_> {
                 goto: return_goto
             };
 
-            let _ = self.impl_state(lr, return_impl)?;
-
+            let return_idx = self.impl_state(lr, return_impl, parrent_goto)?;
             let reduction = self.make_reduction(reduce.clone())?;
+
+            parrent_goto.push(( reduction, return_idx ));
+
             for token in tokens {
                 let t = vecmap!(self, terminals, token.clone());
                 state.lookahead.insert(t, Action::Reduce(reduction));
@@ -216,6 +243,32 @@ impl AutomatonBuilder<'_> {
         }
 
 
+        // catch new returns if stsate uses gotos
+        let mut gotos = Vec::new();
+        let return_catch = {
+            if state_ref.goto_map.len()>0 {
+                &mut gotos
+            } else {
+                parrent_goto
+            }
+        };
+        for (token, position) in &state_ref.shift_map {
+            // implement next state
+            let next_impl = StateImpl {
+                position: position.clone(),
+                goto: state_ref.goto_map.clone().into_iter().collect()
+            };
+            let next = self.impl_state(lr, next_impl, return_catch)?;
+
+            let t = vecmap!(self, terminals, token.clone());
+            state.lookahead.insert(t, Action::Shift(next));
+        }
+
+        if state_ref.goto_map.len()>0 {
+            for (reduction, goto) in gotos {
+            }
+                state.goto.insert(reduction, goto);
+        }
         *self.automaton.states.get_mut(impl_idx).unwrap() = state;
 
         // on reduce insert new implementation
